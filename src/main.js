abaf16a4901b8c4bde37bc75177e92f503bfae07
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2023-11-15 22:16:48 -0300

    feat(aula49e50): add the Tray icon and remove app frame

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 const electron = require('electron');
 // const mm = require('music-metadata');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray, nativeImage } = electron;
 let win;
+let tray = null;
 
 app.on('ready', () => {
+  const icon = nativeImage.createFromPath(
+    '/home/filipereis/Desktop/github/electron_traymusicapp/src/images/iconTemplate.png',
+  );
   win = new BrowserWindow({
     height: 500,
     width: 400,
-    webPreferences: {
+    webPreferen0ces: {
       nodeIntegration: true,
       contextIsolation: false,
     },
+    frame: false,
+    resizable: false,
+    skipTaskbar: true,
   });
 
   win.loadFile('src/index.html');
+  tray = new Tray(icon);
+
+  tray.on('click', () => {
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
+  });
 });
